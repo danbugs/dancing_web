9e3bac381299a1e2e19ac541a9855b6d0e292126
danbugs <danilochiarlone@gmail.com>
2021-08-25 13:15:19 -0300

    addressing issue #11 and fixing dynamic stack buffer overflow problems

--- src/dcw.h
+++ src/dcw.h
@@ -14,3 +14,15 @@
 /** A function macro to allow embedding HTML within C files.
 */
 #define HTML(...) #__VA_ARGS__;
+
+/** A utility function for easy debug. It prints the variable name and its' value.
+    Usage:
+    char *hello_world = "Hello, World!";
+    dbg(hello_world, %d);
+
+    Output:
+    "hello_world: Hello, World!"
+*/
+#define dbg(to_print, formatter) { \
+    printf(#to_print ": " #formatter "\n", to_print); \
+}
